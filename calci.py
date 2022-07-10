3f705ae8e59bd4058e9b1a5d75135ca549241f73
Rondip95 <rohanpawar628@gmail.com>
2022-07-10 12:07:12 +0530

    chahges done

---
+++ calci.py
@@ -0,0 +1,24 @@
+# Simple Calculator
+
+A=100
+print("\n A = ",A)
+B=50
+print("\n B = ",B)
+
+#Addition
+add=A+B
+print("\n Addition of two numbers = ", add)
+
+#Subtraction
+sub=A-B
+print("\n Subtraction of two numbers = ", sub)
+
+#Multiplication
+mul=A*B
+print("\n Multiplication of two numbers = ", mul)
+
+
+#division
+div=A+B
+print("\n Division  of two numbers = ", div)
+
